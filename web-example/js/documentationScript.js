4154a04e732956faf70daac24f09c31e52a620a2
AamuLumi <flokauder@hotmail.com>
2015-04-21 16:59:44 +0200

    Correct some orthographic bugs and add comments to documentationScript.js

--- web-example/js/documentationScript.js
+++ web-example/js/documentationScript.js
@@ -1,18 +1,28 @@
 var colorHoverIdentifier = "#4DC7AF";
 var colorHighlightIdentifier = "#97D17A";
 
+var tooltipOffsetX = 20;
+var tooltipOffsetY = 10;
+
 var highlightedIdentifier = undefined;
 
 var lineNumber = 1;
 
+/**
+ * Reset all highlights
+ */
 function resetHighlightIdentifier() {
-    $(".code-style identifier").each(function (i) {
+    $(".code-style identifier").each(function removeHighlight(i) {
         $(this).css("background", "none");
     });
 
     highlightedIdentifier = undefined;
 }
 
+
+/**
+ * Highlight a specific identifier
+ */
 function highlightIdentifiers(elementText) {
     highlightedIdentifier = elementText;
 
@@ -22,17 +32,23 @@ function highlightIdentifiers(elementText) {
     });
 }
 
+/**
+ * Count the number of <br> in the given tag
+ */
 function numberOfBr(tag) {
     return tag.find('br').length;
 }
 
+/**
+ * Create line numbers element with a code-style element
+ */
 function addLineNumbers(element) {
     var line = 1;
     var endArray = [];
 
     $(".code-style *").each(function () {
         if ($(this).get(0).tagName === "BR") {
-            // Check if this line is an end of bloc
+            // Check if this line is an end of block
             for (var i = 0; i < endArray.length; i++) {
                 if (endArray[i] == line) {
                     endArray.splice(i, 1);
@@ -44,9 +60,9 @@ function addLineNumbers(element) {
 
             element.append(line + "<br>");
             line++;
-        } else if ($(this).attr('class') === "bloc") {
-            // Add bloc if is a bloc
-            var el = $("<bloc>").data("beginning", $(this).data("beginning")).appendTo(element);
+        } else if ($(this).attr('class') === "block") {
+            // Add block if is a block
+            var el = $("<block>").data("beginning", $(this).data("beginning")).appendTo(element);
             endArray.push($(this).data("end"));
 
             // Go to new element
@@ -54,117 +70,145 @@ function addLineNumbers(element) {
         }
     });
 }
+/**
+* Search declaration of an identifier
+*/
+function getDeclarationFor(identifierToFind) {
+    var declaration = undefined;
+
+    $("declaration").each(function (i, v) {
+        if ($(this).data('identifier') == identifierToFind) {
+            declaration = $(this).text();
+            return false;
+        }
+    });
+
+    return declaration;
+}
+
+// ALL ELEMENTS OF CODE-STYLE
+
+$(".code-style *").each(function analyzeHTMLDocument() {
+    // Count number of br in document
+    if ($(this).get(0).tagName === "BR")
+        lineNumber++;
+    // If is a block, add beginning line and end line to properties
+    if ($(this).attr("class") === "block") {
+        var endLineOfBlock = lineNumber + numberOfBr($(this));
+
+        $(this).data('beginning', lineNumber);
+        $(this).data('end', endLineOfBlock);
+    }
+});
 
 
-// DECLARATION
+// DECLARATION //
 
-$(".declaration").each(function (i, v) {
+// Add identifier name to each <declaration></declaration>
+$(".declaration").each(function addIdentifierToDeclaration(i, v) {
     $(this).parent('declaration').data('identifier', $(this).text());
 });
 
 
 // IDENTIFIER
 
-$(".code-style identifier").click(function goToDefinition(){
+// OnClick : go to declaration of identifier
+$(".code-style identifier").click(function goToDefinition() {
     var currentIndentifier = $(this).text();
-    $(".declaration").each(function(){
-        if ($(this).text() === currentIndentifier){
-            $('html,body').animate({scrollTop: $(this).offset().top}, 'medium');
+
+    $(".declaration").each(function searchDeclaration() {
+        if ($(this).text() === currentIndentifier) {
+            // Move to identifier
+            $('html,body').animate({
+                scrollTop: $(this).offset().top
+            }, 'medium');
             return false;
         }
     });
 });
 
-$(".code-style identifier").hover(function highlightAndTooltip() {
-    highlightIdentifiers($(this).text());
+// OnHover : highlight all identifiers with a name
+$(".code-style identifier").hover(
+    function highlightAndTooltip() {
+        // Highlight identifiers
+        highlightIdentifiers($(this).text());
 
-    // Add Tooltip to identifier
-    if ($(this).data('tipText') == undefined) {
         // Search declaration if not defined
-        var identifierToFind = $(this).text();
-        var declaration;
+        if ($(this).data('tipText') == undefined) {
+            var declaration = getDeclarationFor($(this).text());
 
-        $("declaration").each(function (i, v) {
-            if ($(this).data('identifier') == identifierToFind) {
-                declaration = $(this).text();
-                return false;
-            }
+            if (declaration == undefined)
+                $(this).data('tipText', "Not found");
+            else
+                $(this).data('tipText', declaration);
+        }
+
+        var title = $(this).data('tipText');
+
+        // Add tooltip to document
+        $('<p class="identifierTooltip"></p>').text(title).appendTo("body").fadeIn("slow");
+    },
+    function removeHighlightAndTooltip() {
+        // Remove all highlights
+        resetHighlightIdentifier();
+
+        // Remove Tooltip
+        $(".identifierTooltip").remove();
+    })
+
+// OnMouseMove : Move the tooltip
+$(".code-style identifier").mousemove(
+    function moveTooltip(e) {
+        // Determinate new coordonates
+        var mousex = e.pageX + tooltipOffsetX;
+        var mousey = e.pageY + tooltipOffsetY;
+
+        $(".identifierTooltip").css({
+            top: mousey,
+            left: mousex
         });
-
-        if (declaration == undefined)
-            $(this).data('tipText', "Not found");
-        else
-            $(this).data('tipText', declaration);
-    }
-
-    var title = $(this).data('tipText');
-
-    $('<p class="identifierTooltip"></p>').text(title).appendTo("body").fadeIn("slow");
-}, function removeHighlightAndTooltip() {
-
-    resetHighlightIdentifier();
-    // Reinit background color
-    if ($(this).text() === highlightedIdentifier)
-        $(this).css("background", colorHighlightIdentifier);
-    else
-        $(this).css("background", "none");
-
-    // Remove Tooltip
-    $(".identifierTooltip").remove();
-}).mousemove(function moveTooltip(e) {
-    // Move tooltip with mouse
-    var mousex = e.pageX + 20;
-    var mousey = e.pageY + 10;
-    $(".identifierTooltip").css({
-        top: mousey,
-        left: mousex
     });
-});
 
 
 // CODE EXPANDER
 
-$(".code-style *").each(function () {
-    if ($(this).get(0).tagName === "BR")
-        lineNumber++;
-    if ($(this).attr("class") === "bloc") {
-        var endLineOfBloc = lineNumber + numberOfBr($(this));
+// OnClick : collapse/expand next block
+$(".code-expander").click(function activeCodeExpander() {
+    var block = $(this);
 
-        $(this).data('beginning', lineNumber);
-        $(this).data('end', endLineOfBloc);
+    // Search next block
+    while (block.attr("class") != "block" && block != undefined) {
+        block = block.next();
     }
 
-});
-
-$(".code-expander").click(function () {
-    var bloc = $(this);
-
-    while (bloc.attr("class") != "bloc" && bloc != undefined) {
-        bloc = bloc.next();
-    }
-
+    // If block is expanded -> Collapse
     if ($(this).attr('src') == 'img/collapse.png') {
         $(this).attr('src', 'img/expand.png');
-        bloc.slideUp("fast");
-        $(".lineNumber bloc").each(function () {
-            if ($(this).data('beginning') == bloc.data('beginning'))
+        block.slideUp("fast");
+
+        // Collapse blocks in lineNumbers
+        $(".lineNumber block").each(function collapseWithBeginning() {
+            if ($(this).data('beginning') == block.data('beginning'))
                 $(this).slideUp('fast');
         });
-    } else {
+    } else { // Block collapsed -> Expand
         $(this).attr('src', 'img/collapse.png');
-        bloc.slideDown("fast");
-        $(".lineNumber bloc").each(function () {
-            if ($(this).data('beginning') == bloc.data('beginning')) {
-                $(this).slideDown('fast', function () {
+        block.slideDown("fast");
+
+        // Expand blocks in lineNumbers
+        $(".lineNumber block").each(function expandWithBeginning() {
+            if ($(this).data('beginning') == block.data('beginning')) {
+                $(this).slideDown('fast', function checkDisplay() {
                     $(this).css('display', 'inline');
                 });
-
             }
         });
     }
 });
 
 // LINE NUMBER
-$('.lineNumber').each(function (i, v) {
+
+// Add line numbers to HTML Document
+$('.lineNumber').each(function addLineNumbersToDocument() {
     addLineNumbers($(this));
 });
