502758a9e02d20c45eb844559672b7da72b726e5
Nathan Tutard <ntutard@emi.u-bordeaux1.fr>
2015-05-09 10:42:04 +0200

    séparation doc/code

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,4 @@
-bison -d ./c-grammar.y
-flex ./c-grammar.l
-gcc ./c-grammar.tab.c ./lex.yy.c -o ./test-c
+bison -d ./code/c-grammar.y
+flex ./code/c-grammar.l
+gcc ./code/c-grammar.tab.c ./lex.yy.c -o ./test-c
+rm lex.yy.c
