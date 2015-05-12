328bdccda33dbabbfc79b4e1e653fd266ad13248
Nathan Tutard <ntutard@emi.u-bordeaux1.fr>
2015-05-12 11:49:22 +0200

    ajout du fichier test

--- src/doc/compile.sh
+++ src/doc/compile.sh
@@ -1,7 +1,5 @@
 bison -d ./c-grammar.y
-flex -p yy ./c-grammar.l
+flex -P yy ./c-grammar.l
 gcc ./c-grammar.tab.c ./lex.yy.c -o ./test-c
-rm lex.yy.c
-flex -p zz ./second.l
+flex -P zz ./second.l
 gcc ./c-grammar.tab.c ./lex.zz.c -o ./parser
-rm lex.zz.c
